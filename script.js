bec08d1883f7598740321dcd0551068dcc07b149
eliascons <70818338+eliascons@users.noreply.github.com>
2021-09-02 22:07:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+function move(){
+    document.getElementById('sidebar').classList.toggle('active');
+}
+
+// Ill fix these
+function change1(){
+
+        document.getElementById('ele1').classList.toggle('camb');
+}
+
+function change2(){
+
+    document.getElementById('ele2').classList.toggle('camb');
+}
+
+function change3(){
+
+    document.getElementById('ele3').classList.toggle('camb');
+}
+
+function change4(){
+
+    document.getElementById('ele4').classList.toggle('camb');
+}
+function change5(){
+
+    document.getElementById('ele5').classList.toggle('camb');
+}
+
